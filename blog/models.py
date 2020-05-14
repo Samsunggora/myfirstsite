47e816b615547fc98b5ec3c77f86f3edea551a73
samsunggora <samsunggora@gmail.com>
2020-05-14 13:20:32 +0200

    Work fixes

--- blog/models.py
+++ blog/models.py
@@ -3,4 +3,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=150)
+    body = models.TextField()
+    timestamp = models.DateTimeField()
+    def __str__(self):
+        return self.title
