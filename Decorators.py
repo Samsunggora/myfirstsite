22e31f25634d9d2dd9c91cc25466b34f92cd52b6
samsunggora <samsunggora@gmail.com>
2020-05-14 11:11:53 +0200

    add: name setter

--- Decorators.py
+++ Decorators.py
@@ -3,13 +3,21 @@ class Animal(object):
         """show opportunity"""
         print('make new class')
         self.__name = name
+
     @property
     def name(self):
         return self.__name
+
+    @name.setter
+    def name(self, new_name):
+        if new_name == "":
+            print('Error: Name is clear!')
+        else:
+            self.__name = new_name
+
     def talk(self):
         print('Hello my name is :', self.name)
 
+
 x = Animal('barbos')
 x.talk()
-
-
